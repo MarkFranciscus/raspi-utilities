f68229da58c015243c39ce313955ee416d923a4f
MarkFranciscus <mark.franciscus@hotmail.com>
2020-03-03 01:37:40 +0000

    set mode, and float not int

--- fan.py
+++ fan.py
@@ -4,11 +4,12 @@ import signal
 import sys
 import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BCM)
 while True:     # Loop forever
 
     # Read the current temperature
     temp = os.popen('vcgencmd measure_temp').readline()
-    temp = int(''.join(x for x in r if x.isdigit() or x == '.'))
+    temp = float("".join(x for x in temp if x.isdigit() or x == '.'))
     print 'Temperature from vcgencmd: {}'.format(temp)
 
     # Control the fan
